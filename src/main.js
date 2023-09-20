0e1bce06eb4280040d510e3d2f112e15ad8b7d33
luzapedreira <luza.freitas@gmail.com>
2023-09-20 16:07:05 -0300

    pastas para utilização de SPA

--- src/main.js
+++ src/main.js
@@ -3,14 +3,28 @@
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
+import about from "./src/pages/cadastro.js";
 
-loadContent(){ 
-    let contentDiv = document.getElementById("app"); 
-    contentDiv.innerHTML = localização.hash; 
-  } 
-  
-  window.addEventListener("hashchange", function (){ 
-    loadContent(); 
-  }); 
-  
-  carregarConteúdo();
+const main = document.querySelector('#teste');
+const init = () => {
+    window.addEventListener("hashchange", () => {
+        main.innerHTML = "";
+        switch(window.location.hash){
+            case "#login":
+                main.appendChild(login());
+                break;
+            case "#about":
+                main.appendChild(about());
+                break;
+            case "#timeline":
+                main.appendChild(timeline());
+                break;
+            default:
+                main.appendChild(about());
+
+        }
+    }
+}
+window.addEventListener("load", () => {
+  main.appendChild(about());
+})
