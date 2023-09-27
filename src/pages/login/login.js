c2c89c76c7b79304aec469b3ded06d5c8896371f
luzapedreira <luza.freitas@gmail.com>
2023-09-27 18:50:56 -0300

    css login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -11,11 +11,20 @@ export default () => {
       <h2>Sign in</h2>
     </section>
     <form class='input-group'>
-      <input type='email' class='...' placeholder='Email address' id='email' required></input>
-      <label for='email' class='input-group__label'>...</label>
-      <input type='password' class='input-login'placeholder='Password' id='password' required></input>
-      <button class='sign-in-button' id='sign-in'>Sign In</button>
+      <div class='forms'>
+        <input type='email' class='...' id='email' required></input>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label for='email' class='input-group__label'>E-mail address</label>
+      </div>
+      <div class='forms'>
+        <input type='password' class='input-login' id='password' required></input>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label for='password' class='input-group__label'>Password</label>
+      </div>
     </form>
+    <button class='sign-in-button' id='sign-in'>Sign In</button>
     <button id='google' class='google-login'>
       <p>Continue com o Google</p>
     </button>
