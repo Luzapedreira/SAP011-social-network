2c5fc7c60091d63f6ad2d78142ee35e227f1ce8f
luzapedreira <luza.freitas@gmail.com>
2023-09-27 10:07:30 -0300

    html das páginas

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,14 +3,22 @@ export default () => {
   container.classList.add('container');
 
   container.innerHTML = `
-    <section class="page-section">
-      <div class="section-text">
-        <h2> "Login"</h2> 
-        <p>Um teste de como vamos desenvolver a aplicação em SPA.</p>
-        <h3>SERÁ QUE COMPLICA AINDA MAIS?</h3>
-        <p>Só praticando para aprender.</p>
-      <div class="section-text">
+    <section" class="about-login"  >
+    <figure class="logo"> <img src="https://i.pinimg.com/750x/f8/c0/ab/f8c0ab491204f96cde244db38d199540.jpg" alt="logo-fithub" /> </figure>
+      <p id='about-login'> Compartilhe aqui a sua rotina e inspire-se com os hábitos saudáveis de outras pessoas </p>
     </section>
+    <section class="page-login">
+      <h2>Sign in</h2>
+    </section>
+    <form class='input-login'>
+      <input type='email' class='input-login' placeholder='E-mail' id='email' required></input>
+      <input type='password' class='input-login'placeholder='Password' id='password' required></input>
+      <button class='sign-in-button' id='sign-in'>Sign In</button>
+    </form>
+    <button id='google' class='google-login'>
+      <p>Continue com o Google</p>
+    </button>
+
     `;
   return container;
 };
