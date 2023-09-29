a948e71dab501a1d3eefcb0ba446c898799d635d
luzapedreira <luza.freitas@gmail.com>
2023-09-29 14:25:09 -0300

    css das páginas

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,22 +3,32 @@ export default () => {
   container.classList.add('container');
 
   container.innerHTML = `
-    <section" class="about-login"  >
-    
+    <section" class="about-login">
       <p id='about-login'> Compartilhe aqui a sua rotina e inspire-se com os hábitos saudáveis de outras pessoas </p>
     </section>
     <section class="page-login">
       <h2>Sign in</h2>
     </section>
-    <form class='input-login'>
-      <input type='email' class='input-login' placeholder='E-mail' id='email' required></input>
-      <input type='password' class='input-login'placeholder='Password' id='password' required></input>
-      <button class='sign-in-button' id='sign-in'>Sign In</button>
+    <form class='input-group'>
+      <div class='forms'>
+        <input type='email' class='...' id='email' required></input>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label for='email' class='input-group__label'>E-mail address</label>
+      </div>
+      <div class='forms'>
+        <input type='password' class='input-login' id='password' required></input>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label for='password' class='input-group__label'>Password</label>
+      </div>
     </form>
-    <button id='google' class='google-login'>
-      <p>Continue com o Google</p>
-    </button>
-
+    <button class='sign-in-button' id='sign-in'>Sign In</button>
+    <button id='google' class='google-login'><p>Continue with Google</p></button>
+    <div class='register-input>
+      <label for='register-input' class='input-group__label'>Não tem uma conta?</label>
+      <button class='sign-in-button' id='register'>Register</button>
+    </div>
     `;
   return container;
 };
