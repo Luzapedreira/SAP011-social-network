cfd0e58d91c54e8fefa6b38e8d4ae96cc5e0633f
luzapedreira <luza.freitas@gmail.com>
2023-10-04 17:21:00 -0300

    dom entre as páginas de login e registro

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,1 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// eslint-disable-next-line import/no-unresolved
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAV6Ne5cuZqdugVhDFr-amJLB84rDBwdpo',
+  authDomain: 'fithub-woman.firebaseapp.com',
+  databaseURL: 'https://fithub-woman-default-rtdb.firebaseio.com',
+  projectId: 'fithub-woman',
+  storageBucket: 'fithub-woman.appspot.com',
+  messagingSenderId: '382606711960',
+  appId: '1:382606711960:web:6107df72002b098b9e84ed',
+  measurementId: 'G-0YP87ZPXKG',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -3,52 +3,60 @@ export default () => {
   container.classList.add('container');
 
   container.innerHTML = `
-  <section class="about-register>
-    <p id='about-regiser'> Cadastre-se, compartilhe a sua rotina e inspire-se com os hábitos saudáveis de outras pessoas </p>
-  </section>
-  <section class="page-register">
+  <article class='register-page'>
+    <section class='about-register'>
+      <p id='about-regiser'> Cadastre-se, compartilhe a sua rotina e inspire-se com os hábitos saudáveis de outras pessoas </p>
+    </section>
+    <section class='page-register'>
       <h2>Register</h2>
-  </section>
-  <form class='input-register' id='input-register'>
-    <div class='forms'>
-      <span class='highlight'></span>
-      <span class='bar'></span>
-      <input type='text' id='name' class='input-register' required></input>
-      <label for='name' class='input-group__label'>Full name</label>
-    </div>
-    <div class='forms'>
-      <span class='highlight'></span>
-      <span class="bar"></span>
-      <input type='text' id='nickname' class='input-register' required></input>
-      <label for='nickname' class='input-group__label'>Best name</label>
-    </div>
-    <div class='forms'>
-      <span class='highlight'></span>
-      <span class='bar'></span> 
-      <input type='email' id='email' class='input-register' required></input>
-      <label for='email' class='input-group__label'>E-mail</label>
-    </div>
-    <div class='forms'>
-      <span class='highlight'></span>
-      <span class='bar'></span>   
-      <input type='password' id='password' class='input-register' required></input>
-      <label for='password' class='input-group__label'>Password</label>
-    </div>
-    <div class='forms'>
-      <span class='highlight'></span>
-      <span class='bar'></span>  
-      <input id='confirm-password' type='password' class='input-register' required></input>
-      <label for='confirm-password' class='input-group__label'>Confirme password</label>
-    </div>
-    <button id='sign-up-button' class='sign-up-button'>Sign Up</button>
-  </form>
-  <button id='google' class='google-login'>
-    <p>Continue com o Google</p>
-  </button>
-  <div class='login-input>
+    </section>
+    <form class='input-register' id='input-register'>
+      <div class='forms'>
+        <span class='highlight'></span>
+        <span class='bar'></span>
+        <input type='text' id='name' class='input-register' required></input>
+        <label for='name' class='input-group__label'>Full name</label>
+      </div>
+      <div class='forms'>
+        <span class='highlight'></span>
+        <span class="bar"></span>
+        <input type='text' id='nickname' class='input-register' required></input>
+        <label for='nickname' class='input-group__label'>Best name</label>
+      </div>
+      <div class='forms'>
+        <span class='highlight'></span>
+        <span class='bar'></span> 
+        <input type='email' id='email' class='input-register' required></input>
+        <label for='email' class='input-group__label'>E-mail</label>
+      </div>
+      <div class='forms'>
+        <span class='highlight'></span>
+        <span class='bar'></span>   
+        <input type='password' id='password' class='input-register' required></input>
+        <label for='password' class='input-group__label'>Password</label>
+      </div>
+      <div class='forms'>
+        <span class='highlight'></span>
+        <span class='bar'></span>  
+        <input id='confirm-password' type='password' class='input-register' required></input>
+        <label for='confirm-password' class='input-group__label'>Confirme password</label>
+      </div>
+      <button id='sign-up-button' class='sign-up-button'>Sign Up</button>
+    </form>
+    <button id='google' class='google-login'>
+      <p>Continue com o Google</p>
+    </button>
+    <div class='login-input'>
       <label for='login-input' class='input-group__label'>Já tem uma conta?</label>
-      <button class='sign-in-button' id='login'>Login</button>
+      <button class='sign-in-button' id='sign-in-button'>Login</button>
     </div>
+  </article>
   `;
   return container;
 };
+// Função ir para a página de login
+function goLoginPage() {
+  window.location.href = 'pages/login/login.js';
+}
+
+document.getElementById('sign-in-button').addEventListener('click', goLoginPage);
