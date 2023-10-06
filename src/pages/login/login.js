f00fe734465f235b81ffb562a70e004798fefc38
Dahbarbara <dahbarbara_@hotmail.com>
2023-10-06 15:03:45 -0300

    login sem aparecer

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,33 +1,25 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-//import  from '../../firebase/firebase-config.js';
+// import  from '../../firebase/firebase-config.js';
 import { firebaseConfig } from '../../firebase/firebase-config.js';
-export default () => {
-  // import 'firebase/auth'; // Importe a parte de autenticação do Firebase
 
-  // Função para fazer login com o Google
-  function LoginGoogle() {
-    const provider = new GoogleAuthProvider(firebaseConfig);
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // Sucesso - usuário logado com o Google
-        const user = result.user;
-        // eslint-disable-next-line no-console
-        console.log('Usuário logado com o Google:', user);
-      })
-      .catch((error) => {
-        // Erro durante o login com o Google
-        // eslint-disable-next-line no-console
-        console.error('Erro durante o login com o Google:', error);
-      });
-  }
+// Função para fazer login com o Google
+function LoginGoogle() {
+  const provider = new GoogleAuthProvider(firebaseConfig);
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // Sucesso - usuário logado com o Google
+      const user = result.user;
+      // eslint-disable-next-line no-console
+      console.log('Usuário logado com o Google:', user);
+    })
+    .catch((error) => {
+      // Erro durante o login com o Google
+      // eslint-disable-next-line no-console
+      console.error('Erro durante o login com o Google:', error);
+    });
+}
 
-  // Adicione o evento de clique ao botão "google" para fazer login com o Google
-  const googleButton = document.querySelector('#google');
-  googleButton.addEventListener('click', () => {
-    LoginGoogle();
-  });
-};
 // Resto do seu código HTML e lógica permanece inalterado...
 
 const container = document.createElement('main');
@@ -50,6 +42,8 @@ container.innerHTML = `
     <p>Continue com o Google</p>
   </button>
 `;
-
-// Adicione o container à página
-document.body.appendChild(container);
+// Adicione o evento de clique ao botão "google" para fazer login com o Google
+const googleButton = container.querySelector('#google');
+googleButton.addEventListener('click', () => {
+  LoginGoogle();
+});
